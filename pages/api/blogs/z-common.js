27201ea375a9196206bc2c0938227d5553f4b1ab
nickskies <nicholas.saravia@worldacceptance.com>
2021-08-19 14:59:01 -0400

    vercel secret

--- pages/api/blogs/z-common.js
+++ pages/api/blogs/z-common.js
@@ -2,5 +2,8 @@
 // this file will only have common methods shared between api-endpoints
 
 export const getJson = () => {
+    if (process.env.secret === "super-secret")
+        return { name: 'John Yay' }
+        
     return { name: 'John Doe' }
 }
